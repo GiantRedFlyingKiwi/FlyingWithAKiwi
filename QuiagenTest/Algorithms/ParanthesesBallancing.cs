d66d62859463729cb2107554957a55b852a8f34d
Tibor Foldi <giantredflyingkiwi@gmail.com>
2024-03-27 23:44:01 +0200

    Parantheses Balancing Algorihm with Stack

---
+++ QuiagenTest/Algorithms/ParanthesesBallancing.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class ParanthesesBallancing
+    {
+         
+
+        private List<char> ConvertStringToCharList(string input)
+        {
+            List<char> charList = new List<char>();
+
+            foreach (char c in input)
+            {
+                charList.Add(c);
+            }
+
+            return charList;
+        }
+
+        public bool CheckParanthesesBalancing(string input)
+        {
+            List<char> chars = ConvertStringToCharList(input);
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in chars)
+            {
+                if (c == '(')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false; // There's a closing parenthesis without a corresponding opening parenthesis
+                    }
+                    stack.Pop();
+                }
+            }
+            return stack.Count == 0; // True if stack is empty, False otherwise
+        }
+    }
+}
---
+++ QuiagenTest/QuiagenTest/AlgorithmsTests.cs
@@ -0,0 +1,42 @@
+﻿using Algorithms;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiagenTest
+{
+    public class AlgorithmsTests
+    {
+        [Test]
+        public void TestBallancingPositive()
+        {
+            ParanthesesBallancing ballancing = new ParanthesesBallancing();
+            ClassicAssert.IsTrue(ballancing.CheckParanthesesBalancing("(if (zero? x) max (/ 1 x))"));
+        }
+
+        [Test]
+        public void TestBallancingPositive2()
+        {
+            ParanthesesBallancing ballancing = new ParanthesesBallancing();
+            ClassicAssert.IsTrue(ballancing.CheckParanthesesBalancing("I told him (that it’s not (yet) done). (But he wasn’t listening)"));
+        }
+
+        [Test]
+        public void TestBallancingNegative()
+        {
+            ParanthesesBallancing ballancing = new ParanthesesBallancing();
+            ClassicAssert.IsFalse(ballancing.CheckParanthesesBalancing(":-)"));
+        }
+
+        [Test]
+        public void TestBallancingNegative2()
+        {
+            ParanthesesBallancing ballancing = new ParanthesesBallancing();
+            ClassicAssert.IsFalse(ballancing.CheckParanthesesBalancing("())("));
+        }
+    }
+}
